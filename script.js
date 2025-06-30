60da2a0733cf079fb3a91755f118f6873104a297
Sridhanam <dharsri255@gmail.com>
2025-06-30 09:05:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function highlight() {
     //Write your code here
-
-
+	Array.from(document.getElementsByTagName('strong')).forEach(strongElement => {
+		strongElement.style.color = "green";
+	});
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+    //Write your code here 
+    Array.from(document.getElementsByTagName('strong')).forEach(strongElement => {
+		strongElement.style.color = "black";
+	});
 }
